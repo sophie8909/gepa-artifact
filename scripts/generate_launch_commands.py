6f9e09083ec8bf5bf902dafc3cd5144bd2a5da02
sophie8909 <34826797+sophie8909@users.noreply.github.com>
2025-11-27 13:38:37 +0800

    Revert "setup"
    This reverts commit 867f9af80badd551d36e77d841611d1e2a500684.

--- scripts/generate_launch_commands.py
+++ scripts/generate_launch_commands.py
@@ -1,5 +1,4 @@
 import json
-import os
 from .run_experiments import get_benchmarks, get_optimizers
 from .experiment_configs import LM_CONFIGS
 
@@ -18,7 +17,7 @@ for seed in SEEDS:
         # Get the instantiated benchmark just for the name
         benchmark = benchmark_meta.benchmark(dataset_mode=dataset_mode) if dataset_mode else benchmark_meta.benchmark()
         benchmark_name = benchmark_meta.name or benchmark.__class__.__name__
-        num_threads = getattr(benchmark_meta, 'num_threads', None) or os.cpu_count()
+        num_threads = getattr(benchmark_meta, 'num_threads', None) or 32
 
         for program_idx, program in enumerate(benchmark_meta.program):
             prog_name = getattr(program, "_name", program.__class__.__name__)
